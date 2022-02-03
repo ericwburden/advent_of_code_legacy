7546feaaa80c5f8809a1f68347d53ef7550d0aa2
Eric Burden <eric@ericburden.dev>
2022-02-03 05:43:17 -0600

    Refactor 2015, Day 12
    Using multiple functions with different input types instead of a single
    function with if statements.

--- src/Year2015/Day12/part01.jl
+++ src/Year2015/Day12/part01.jl
@@ -2,25 +2,25 @@
     extract_numbers(json, numbers = [])
 
 Recursively search a JSON input, adding each number found to `numbers` and 
-returning the result.
+returning the result. Employs function overloading to dispatch on the type
+of the input.
 """
-function extract_numbers(json, numbers = [])
-    json isa Int && push!(numbers, json)
-
-    if json isa Vector
-        for value in json
-            extract_numbers(value, numbers)
-        end
-    end
-
-    if json isa Dict
-        for value in values(json)
-            extract_numbers(value, numbers)
-        end
-    end
-
+function extract_numbers(number::Int, numbers=[])
+    push!(numbers, number)
     return numbers
 end
 
+function extract_numbers(vector::Vector, numbers=[])
+    foreach(x -> extract_numbers(x, numbers), vector)
+    return numbers
+end
+
+function extract_numbers(dict::Dict, numbers=[])
+    foreach(x -> extract_numbers(x, numbers), values(dict))
+    return numbers
+end
+
+extract_numbers(::String, ::Vector) = nothing
+
 "Extract and sum all the numbers from `input`"
-part1(input) = extract_numbers(x) |> sum
+part1(input) = extract_numbers(input) |> sum
--- src/Year2015/Day12/part02.jl
+++ src/Year2015/Day12/part02.jl
@@ -3,26 +3,26 @@
 
 Recursively search a JSON input, adding each number found to `numbers` and 
 returning the result. Skip any `Dict` node (and its children) where any of 
-the dictionary values is "red".
+the dictionary values is "red". Uses function overloading to dispatch based
+on the input type.
 """
-function extract_numbers2(json, numbers = [])
-    json isa Int && push!(numbers, json)
-    
-    if json isa Vector
-        for value in json
-            extract_numbers2(value, numbers)
-        end
-    end
-
-    if json isa Dict
-        "red" in values(json) && return numbers
-        for value in values(json)
-            extract_numbers2(value, numbers)
-        end
-    end
-
+function extract_numbers2(number::Int, numbers=[])
+    push!(numbers, number)
     return numbers
 end
 
+function extract_numbers2(vector::Vector, numbers=[])
+    foreach(x -> extract_numbers2(x, numbers), vector)
+    return numbers
+end
+
+function extract_numbers2(dict::Dict, numbers=[])
+    "red" in values(dict) && return numbers
+    foreach(x -> extract_numbers2(x, numbers), values(dict))
+    return numbers
+end
+
+extract_numbers2(::String, ::Vector) = nothing
+
 "Extract and sum all the numbers from `input`, excepting the 'red' ones"
-part2(input) = extract_numbers2(x) |> sum
+part2(input) = extract_numbers2(input) |> sum
